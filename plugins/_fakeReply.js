58065f08a88b91a32c8e62ab66ed78d0d29c0cb9
EazzyHacks <yhair7200@gmail.com>
2025-08-19 22:56:36 +0100

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-let name = `💮 🌹𝐓𝐡𝐞-𝐌𝐢𝐤𝐮𝐁𝐨𝐭-𝐌𝐃🌹 💮`
-let imagenes = ["https://qu.ax/ARhkT.jpg",
-"https://qu.ax/VdOqJ.jpg",
-"https://qu.ax/VdOqJ.jpg",
-"https://qu.ax/VdOqJ.jpg",]
+let name = `👻 𝗚𝗵𝗼𝘀𝘁 𝗠𝗼𝗱𝗲 𝗕𝘆 👻`
+let imagenes = ["https://qu.ax/rZzaU.jpg",
+"https://qu.ax/rZzaU.jpg",
+"https://qu.ax/rZzaU.jpg",
+"https://qu.ax/rZzaU.jpg",]
 
 let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 
@@ -13,7 +13,7 @@ global.rcanal = {
  contextInfo: {
              isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363403726798403@newsletter",
+      newsletterJid: "120363419947391620@newsletter",
       serverMessageId: 100,
       newsletterName: name,
    }, 
@@ -33,12 +33,12 @@ global.rcanal = {
 
 
 global.icono = [ 
-'https://qu.ax/yyCo.jpeg',
-'https://qu.ax/yyCo.jpeg',
-'https://qu.ax/qJch.jpeg',
-'https://qu.ax/qJch.jpeg',
-'https://qu.ax/CHRS.jpeg',
-'https://qu.ax/CHRS.jpeg',
+'https://qu.ax/rZzaU.jpg',
+'https://qu.ax/rZzaU.jpg',
+'https://qu.ax/rZzaU.jpg',
+'https://qu.ax/rZzaU.jpg',
+'https://qu.ax/rZzaU.jpg',
+'https://qu.ax/rZzaU.jpg',
 ].getRandom()
 
         global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./storage/img/catalogo.png'), thumbnail: fs.readFileSync('./storage/img/catalogo.png'), sendEphemeral: true } } }
@@ -62,7 +62,7 @@ global.icono = [
     contextInfo: {
             isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363403726798403@newsletter",
+      newsletterJid: "120363419947391620@newsletter",
       serverMessageId: 100,
       newsletterName: name,
     },
