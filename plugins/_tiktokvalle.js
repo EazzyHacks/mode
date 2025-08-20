6fbd05b9caa40d32a1c33dd3aa82fe5456cee36d
EazzyHacks <yhair7200@gmail.com>
2025-08-20 01:44:35 -0500

    Update _tiktokvalle.js

--- plugins/_tiktokvalle.js
+++ plugins/_tiktokvalle.js
@@ -1,17 +1,34 @@
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let res = await tiktokvalle[Math.floor(Math.random() * tiktokvalle.length)]
-await m.react('💔')
+await m.react('🗣')
 conn.sendMessage(m.chat, { video: { url: res }, caption: `» 𝗩𝗔𝗟𝗟𝗘𝗡𝗔𝗧𝗢𝗦 𝗧𝗜𝗞𝗧𝗢𝗞 💔` }, { quoted: m })}
-handler.help = ['tiktokvalle']
+handler.help = ['tiktokhot']
 handler.tags = ['random']
-handler.command = /^(tiktokvalle)$/i
+handler.command = /^(tiktokhot)$/i
 export default handler
 global.tiktokvalle = [
-"https://telegra.ph/file/c33f7f1fafb6d1b9f61c8.mp4",
-"https://telegra.ph/file/639bc84a65e597a07073c.mp4",
-  "https://telegra.ph/file/0aa955aaa78a206b45170.mp4",
-  "https://telegra.ph/file/a40c752c7d61e81b47e99.mp4",
-  "https://telegra.ph/file/c33f7f1fafb6d1b9f61c8.mp4",
-"https://telegra.ph/file/9f895c560dab513d020e5.mp4"
+"https://files.catbox.moe/qp0xx5.mp4",
+"https://files.catbox.moe/bs834q.mp4",
+  "https://files.catbox.moe/7flsvo.mp4",
+  "https://files.catbox.moe/7flsvo.mp4",
+  "https://files.catbox.moe/l3lgtn.mp4",
+  "https://files.catbox.moe/dnq649.mp4",
+  "https://files.catbox.moe/0hm5e6.mp4",
+  "https://files.catbox.moe/d6ysm2.mp4",
+  "https://files.catbox.moe/0m3zlk.mp4",
+  "https://files.catbox.moe/9wk5jm.mp4",
+  "https://files.catbox.moe/3vypgu.mp4",
+  "https://files.catbox.moe/vo9hdh.mp4",
+  "https://files.catbox.moe/923uiu.mp4",
+  "https://files.catbox.moe/qvtspf.mp4",
+  "https://files.catbox.moe/embd2t.mp4",
+  "https://files.catbox.moe/3at7m3.mp4",
+  "https://files.catbox.moe/85v06n.mp4",
+"https://files.catbox.moe/6bp9ls.mp4",
+"https://files.catbox.moe/148602.mp4",
+  "https://files.catbox.moe/ij2ix1.mp4",
+  "https://files.catbox.moe/raqt0n.mp4",
+  "https://files.catbox.moe/u2w420.mp4",
+"https://files.catbox.moe/3qh4j7.mp4"
 ]
